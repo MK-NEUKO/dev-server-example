2b88e55fcf83811310dd5b5f91069dd00ef0f127
Michael Koch <m.koch@neuko-devment.com>
2024-12-17 22:07:02 +0100

    Enhance setup and configuration
    - Modified `Program.cs` to enhance the setup of the `DistributedApplication` builder:
      - Assigned `weatherApi` project to a variable and configured with external HTTP endpoints.
      - Added a new NPM application `devServerClient` with several configurations, including references to `weatherApi`, waiting for `weatherApi`, setting an HTTP endpoint, adding external HTTP endpoints, and publishing as a Docker file.
    - Cleaned up `.gitignore` by removing lines used to ignore various files and directories.
    - Updated `applicationUrl` for the `https` configuration in `launchSettings.json` from `https://localhost:7131` to `https://localhost:5001`.

--- src/DevServer.AppHost/Program.cs
+++ src/DevServer.AppHost/Program.cs
@@ -1,5 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.WeatherApi>("weatherapi");
+var weatherApi = builder.AddProject<Projects.WeatherApi>("weatherapi")
+    .WithExternalHttpEndpoints();
+
+builder.AddNpmApp("devServerClient", "../DevServerClient")
+    .WithReference(weatherApi)
+    .WaitFor(weatherApi)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
