845077d68cb7bd1c22bba86f4c64a958105bcbc5
Michael Koch <m.koch@neuko-devment.com>
2025-01-25 12:18:55 +0100

    Refactor forecast navigation components; update HTML structure, improve data binding, and add date conversion logic

--- src/DevServerClient/src/app/Pages/weather-forecast/Components/forecast-nav-item/forecast-nav-item.component.ts
+++ src/DevServerClient/src/app/Pages/weather-forecast/Components/forecast-nav-item/forecast-nav-item.component.ts
@@ -16,6 +16,8 @@ export class ForecastNavItemComponent implements OnInit {
 
   public forecastPerDay!: ForecastDataPerDay;
   itemIndex = input<number>(0);
+  weekdays: string[] = [];
+  dates: string[] = [];
 
   constructor(private weatherForecastDataService: WeatherForecastDataService) {
   }
@@ -26,7 +28,36 @@ export class ForecastNavItemComponent implements OnInit {
         this.forecastPerDay = data;
       }
     });
+
+    this.convertDateToWeekday(this.forecastPerDay.time);
+    this.convertDateToDates(this.forecastPerDay.time);
   }
 
+  convertDateToWeekday(date: string[]): void {
+    date.forEach(element => {
+      const weekday = new Date(element).toLocaleDateString('en-US', { weekday: 'short' });
+      this.weekdays.push(weekday);
+    });
+  }
 
+  convertDateToDates(date: string[]): void {
+    date.forEach((element, index) => {
+      const day = new Date(element).toLocaleDateString('en-US', { day: 'numeric' });
+      const month = new Date(element).toLocaleDateString('en-US', { month: 'short' });
+      const date = `${day}. ${month}`;
+      if (index === 0) {
+        this.dates.push('Today');
+      }
+      else if (index === 1) {
+        this.dates.push('Tomorrow');
+      }
+      else {
+        this.dates.push(date);
+      }
+    });
+  }
+
+  convertPicToCode(date: number[]): void {
+
+  }
 }
