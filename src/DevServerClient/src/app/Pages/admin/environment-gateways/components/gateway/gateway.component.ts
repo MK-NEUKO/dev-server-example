3e445abdd29c67815a57e7176e30579a7eb04c75
Michael Koch <m.koch@neuko-devment.com>
2025-02-21 17:55:22 +0100

    Add new dependencies and update GatewayComponent
    Updated package-lock.json and package.json to include new dependencies: ang-jsoneditor, jsoneditor, ace-builds, javascript-natural-sort, jmespath, json-source-map, jsonrepair, picomodal, and vanilla-picker.
    
    Modified gateway.component.ts to:
    - Import OnInit and OnDestroy from @angular/core.
    - Import EnvironmentService and EnvironmentGatewayDataService.
    - Import GatewayConfiguration from models path.
    - Import Subscription from rxjs.
    - Implement OnInit and OnDestroy interfaces.
    - Add private subscription and gatewayConfiguration properties.
    - Inject EnvironmentService and EnvironmentGatewayDataService in constructor.
    - Implement ngOnInit to subscribe to configuration data.
    - Implement ngOnDestroy to unsubscribe from all subscriptions.

--- src/DevServerClient/src/app/Pages/admin/environment-gateways/components/gateway/gateway.component.ts
+++ src/DevServerClient/src/app/Pages/admin/environment-gateways/components/gateway/gateway.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { EnvironmentService } from '../../../../../services/environment-gateway/environment-gateway.service';
+import { EnvironmentGatewayDataService } from '../../../../../services/environment-gateway/environment-gateway-data.service';
+import { GatewayConfiguration } from '../../../../../models/environment-gateway/gatewayConfiguration';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-gateway',
@@ -6,6 +10,27 @@ import { Component } from '@angular/core';
   templateUrl: './gateway.component.html',
   styleUrl: './gateway.component.css'
 })
-export class GatewayComponent {
+export class GatewayComponent implements OnInit, OnDestroy {
+  private subscription: Subscription = new Subscription();
+  private gatewayConfiguration?: GatewayConfiguration;
 
+
+  constructor(
+    private environmentService: EnvironmentService,
+    private environmentDataService: EnvironmentGatewayDataService
+  ) { }
+
+  ngOnInit() {
+    this.subscription.add(this.environmentService.getConfigurations().subscribe((data) => {
+      this.environmentDataService.setGatewayConfiguration(data);
+    }));
+
+    this.subscription.add(this.environmentDataService.getGatewayConfiguration().subscribe((data) => {
+      this.gatewayConfiguration = data ?? this.environmentDataService.getDefaultGatewayConfiguration();
+    }));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
