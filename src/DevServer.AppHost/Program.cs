ead289aaff6d331dfdf3253b5320dfa9ee83154f
Michael Koch <m.koch@neuko-devment.com>
2025-03-05 23:07:21 +0100

    Refactor EnvironmentGateway into modular projects
    Updated the solution to include new projects: EnvironmentGatewayApi, EnvironmentGatewayDomain, EnvironmentGatewayApplication, EnvironmentGatewayInfrastructure, and SharedKernel. Removed the previous EnvironmentGateway project reference and adjusted the Global section accordingly.
    
    Added a project reference to EnvironmentGatewayApi in DevServer.AppHost.csproj. Modified Program.cs to integrate the new API and removed outdated gateway configurations.
    
    Updated the Dockerfile to support the new project structure and created new HTTP files for EnvironmentGatewayApi.
    
    Introduced WeatherForecast and WeatherForecastController classes in the new API. Updated appsettings files for logging configurations.
    
    These changes enhance the modularity and maintainability of the EnvironmentGateway functionality.

--- src/DevServer.AppHost/Program.cs
+++ src/DevServer.AppHost/Program.cs
@@ -1,29 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var productionGateway = builder.AddProject<Projects.EnvironmentGateway>("productionGateway")
-    .WithHttpEndpoint(port: 5500)
-    .WithHttpsEndpoint(port: 7700)
-    .WithExternalHttpEndpoints()
-    .WithArgs("Production Gateway");
-    
 
-
-var stagingGateway = builder.AddProject<Projects.EnvironmentGateway>("stagingGateway")
-    .WithHttpEndpoint(port: 5501)
-    .WithHttpsEndpoint(port: 7701)
-    .WithArgs("Staging Gateway");
 
 var weatherApi = builder.AddProject<Projects.WeatherForecastApi>("weatherApi")
     .WithExternalHttpEndpoints();
 
 builder.AddNpmApp("devServerClient", "../DevServerClient")
     .WithReference(weatherApi)
-    .WithReference(productionGateway)
-    .WithReference(stagingGateway)
     .WaitFor(weatherApi)
     .WithHttpEndpoint(env: "DEV_SERVER_CLIENT_PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
+
+
+builder.AddProject<Projects.EnvironmentGatewayApi>("environmentgatewayapi");
 
 
 await builder.Build().RunAsync();
