029ce6d7c14dfb5d16fc1c0ed6dedb3c4c9d6a05
Michael Koch <m.koch@neuko-devment.com>
2025-03-18 22:59:20 +0100

    Add SetDestination class with POST endpoint for address
    Introduces the `SetDestination` class in the
    `EnvironmentGatewayApi.Endpoints.GatewayConfiguration` namespace.
    This class implements the `IEndpoint` interface and defines a
    POST endpoint at `/set-address/{address}` that returns a
    JSON response with the provided address.

---
+++ src/EnvironmentGateway/EnvironmentGateway.Api/Endpoints/GatewayConfiguration/SetDestination.cs
@@ -0,0 +1,14 @@
+﻿namespace EnvironmentGatewayApi.Endpoints.GatewayConfiguration;
+
+public class SetDestination : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/set-address/{address}", (string address, CancellationToken cancellationToken) =>
+        {
+            var value = address;
+
+            return Results.Json(address);
+        });
+    }
+}
