d3c621b403dd0fdad3bd2463f20a08eccdd4e819
Michael Koch <m.koch@neuko-devment.com>
2025-03-18 23:57:33 +0100

    Add RuntimeConfigurator and IRuntimeConfigurator interface
    Introduce RuntimeConfigurator class to manage destination address changes. Define IRuntimeConfigurator interface for consistent runtime configuration functionality.

---
+++ src/EnvironmentGateway/EnvironmentGateway.Api/GatewayConfiguration/RuntimeConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using EnvironmentGatewayApi.GatewayConfiguration.Abstractions;
+using Yarp.ReverseProxy.Configuration;
+
+namespace EnvironmentGatewayApi.GatewayConfiguration;
+
+public class RuntimeConfigurator(
+    IProxyConfigProvider configurationProvider) 
+    : IRuntimeConfigurator
+{
+    public void ChangeDestinationAddress(string address)
+    {
+        var config = configurationProvider.GetConfig();
+        
+    }
+}
