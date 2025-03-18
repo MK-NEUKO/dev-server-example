3a0b4878208b68396f0a0df421f0073155a5b1ca
Michael Koch <m.koch@neuko-devment.com>
2025-03-18 12:47:45 +0100

    Refactor project structure and add EnvironmentGateway.Api
    - Removed several projects related to EnvironmentGateway.
    - Added new project: EnvironmentGateway.Api.
    - Updated solution and project files to reflect new structure.
    - Implemented routing and configuration for reverse proxy using YARP.
    - Created new interfaces and classes for endpoint management.
    - Updated Dockerfile and launch settings for new project setup.
    - Adjusted appsettings for logging and reverse proxy configurations.

--- src/EnvironmentGateway/EnvironmentGatewayDomain/ReverseProxy/ChangeToken.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EnvironmentGatewayDomain.NewFolder;
-
-public sealed class ChangeToken
-{
-    public bool HasChanged { get; set; }
-    public bool ActiveChangedCallbacks { get; set; }
-}
