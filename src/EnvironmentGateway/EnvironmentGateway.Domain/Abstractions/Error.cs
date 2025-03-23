e9adf71b550343dc51eee991bf05403ddd371fa7
Michael Koch <m.koch@neuko-devment.com>
2025-03-23 23:45:38 +0100

    Add IUnitOfWork and IGatewayConfigurationRepository interfaces
    Introduced two new interfaces: `IUnitOfWork` for asynchronous save operations and `IGatewayConfigurationRepository` for managing `GatewayConfiguration` entities, including retrieval by ID and addition of new instances.
    
    Add error handling and repository interfaces
    
    Introduce `Error` record for error states, `IUnitOfWork` interface for async data management, and `Result` classes for operation outcomes. Add `IGatewayConfigurationRepository` for managing gateway configurations.

---
+++ src/EnvironmentGateway/EnvironmentGateway.Domain/Abstractions/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace EnvironmentGateway.Domain.Abstractions;
+
+public record Error(string Code, string Name)
+{
+    public static Error None = new(String.Empty, String.Empty);
+
+    public static Error NullValue = new("Error.NullValue", "Null Value was provided");
+}
---
+++ src/EnvironmentGateway/EnvironmentGateway.Domain/GatewayConfiguration/IGatewayConfigurationRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace EnvironmentGateway.Domain.GatewayConfiguration;
+
+public interface IGatewayConfigurationRepository
+{
+    Task<GatewayConfiguration?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    void Add(GatewayConfiguration gatewayConfiguration);
+}
