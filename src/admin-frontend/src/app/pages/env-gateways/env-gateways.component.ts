ba77e07a03f8b80e884ee9fce989a48a8be8cedb
Michael Koch <m.koch@neuko-devment.com>
2025-05-02 14:26:55 +0200

    Add syntax highlighting with highlight.js and ngx-highlightjs
    - Introduced `highlight.js` and `ngx-highlightjs` as dependencies.
    - Configured `provideHighlightOptions` in `app.config.ts`.
    - Updated `env-gateways.component.html` to use highlighted code blocks.
    - Imported necessary modules in `env-gateways.component.ts` and added styles.
    - Added new CSS styles for code highlighting in `styles.css`.
    - Updated `settings.json` to include `highlightjs` in the spell checker.

--- src/admin-frontend/src/app/pages/env-gateways/env-gateways.component.ts
+++ src/admin-frontend/src/app/pages/env-gateways/env-gateways.component.ts
@@ -1,12 +1,22 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GatewayDataService } from '../../services/env-gateway/gateway-data.service';
+import { Highlight } from 'ngx-highlightjs';
+import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
+
+import 'highlight.js/styles/androidstudio.min.css';
 
 @Component({
   selector: 'app-env-gateways',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    Highlight,
+    HighlightLineNumbers
+  ],
   templateUrl: './env-gateways.component.html',
-  styleUrl: './env-gateways.component.css'
+  styleUrls: [
+    './env-gateways.component.css'
+  ]
 })
 export class EnvGatewaysComponent {
 
@@ -14,11 +24,10 @@ export class EnvGatewaysComponent {
   public currentConfig = this.gatewayDataService.getCurrentConfig();
   private destinationUrl = 'https://localhost:5201/';
 
+
   constructor() { }
 
   destinationTest() {
     window.open(this.destinationUrl, '_blank');
   }
 }
-
-
