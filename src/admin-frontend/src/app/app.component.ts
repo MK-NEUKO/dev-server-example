8133e0e124d161aeb726d8d5e57322c43ea69c21
Michael Koch <m.koch@neuko-devment.com>
2025-05-04 23:01:02 +0200

    Refactor app structure and enhance sidebar navigation
    Removed the `app-env-gateways` component and its import from `app.component.ts`. Added new routes for `DashboardComponent` and `EnvGatewaysComponent` in `app.routes.ts`. Revamped the sidebar in `sidebar.component.html` to include navigation links and updated styles in `sidebar.component.css`. Included `RouterModule` in `sidebar.component.ts` for improved routing capabilities. These changes aim to streamline the codebase and enhance user experience.

--- src/admin-frontend/src/app/app.component.ts
+++ src/admin-frontend/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
-import { EnvGatewaysComponent } from "./pages/env-gateways/env-gateways.component";
 import { ThemeService } from './services/theme/theme.service';
 import { SidebarComponent } from "./layout/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, EnvGatewaysComponent, SidebarComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
