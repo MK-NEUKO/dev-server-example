e52cf5fc2a8ae9bcc6ca08d749c9219b040f86d4
Michael Koch <m.koch@neuko-devment.com>
2025-06-06 16:39:45 +0200

    Refactor routes handling in config editor to array
    Updated the form structure in `config-editor.component.html` to use `formArrayName` for routes, allowing for dynamic handling of multiple routes. Adjusted HTML for clarity and proper formatting.
    
    Modified `config-editor.component.ts` to include `NgFor` for iterating over routes and changed the `config` FormGroup to utilize a FormArray. Updated the `updateConfigForm` method to correctly access and update route controls from the FormArray.

--- src/admin-frontend/src/app/pages/env-gateways/config-editor/config-editor.component.ts
+++ src/admin-frontend/src/app/pages/env-gateways/config-editor/config-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, effect, inject } from '@angular/core';
+import { NgFor } from '@angular/common';
 import { FormGroup, FormControl, ReactiveFormsModule, FormArray } from '@angular/forms';
 import { GatewayDataService } from '../../../services/env-gateway/gateway-data.service';
 import { GatewayConfig } from '../../../models/gateway-config/gateway-config.model';
@@ -7,6 +8,7 @@ import { GatewayConfig } from '../../../models/gateway-config/gateway-config.mod
   selector: 'app-config-editor',
   imports: [
     ReactiveFormsModule,
+    NgFor
   ],
   templateUrl: './config-editor.component.html',
   styleUrl: './config-editor.component.css'
@@ -29,13 +31,16 @@ export class ConfigEditorComponent {
 
   config = new FormGroup({
     configName: new FormControl('not init'),
-    routes: new FormGroup({
-      routeName: new FormControl('not init'),
-      clusterName: new FormControl('not init'),
-      match: new FormGroup({
-        path: new FormControl('not init'),
+    routes: new FormArray([
+      new FormGroup({
+        routeName: new FormControl('not init'),
+        clusterName: new FormControl('not init'),
+        match: new FormGroup({
+          path: new FormControl('not init'),
+        }),
       }),
-    }),
+    ]),
+
     clusters: new FormGroup({
       clusterName: new FormControl('not init'),
       destination: new FormGroup({
@@ -52,9 +57,11 @@ export class ConfigEditorComponent {
     this.config.get('clusters.clusterName')?.setValue(this.configData?.clusters[0].clusterName || 'error');
     this.config.get('clusters.destination.destinationName')?.setValue(this.configData?.clusters[0].destinations[0].destinationName || 'error');
     this.config.get('clusters.destination.address')?.setValue(this.configData?.clusters[0].destinations[0].address || 'error');
-    this.config.get('routes.routeName')?.setValue(this.configData?.routes[0].routeName || 'error');
-    this.config.get('routes.clusterName')?.setValue(this.configData?.routes[0].clusterName || 'error');
-    this.config.get('routes.match.path')?.setValue(this.configData?.routes[0].match.path || 'error');
+    const routesArray = this.config.get('routes') as FormArray;
+    const firstRoute = routesArray.at(0) as FormGroup;
+    firstRoute.get('routeName')?.setValue(this.configData?.routes[0].routeName || 'error');
+    firstRoute.get('clusterName')?.setValue(this.configData?.routes[0].clusterName || 'error');
+    firstRoute.get('match.path')?.setValue(this.configData?.routes[0].match.path || 'error');
   }
 
 }
