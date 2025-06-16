cfd8f4300748d5c55641267624a2e8c2a3034ed2
Michael Koch <m.koch@neuko-devment.com>
2025-06-16 23:50:43 +0200

    Refactor package management and project references
    - Updated `Directory.Build.props` to enhance `SonarAnalyzer.CSharp` package management with `PrivateAssets` and `IncludeAssets`.
    - Removed `WeatherForecastApi` project reference from the solution, indicating its exclusion.
    - Modified `DevServer.AppHost.csproj` and other project files to eliminate specific version numbers for packages, enabling centralized version management via `Directory.Packages.props`.
    - Removed `weatherApi` reference in `Program.cs`, reflecting architectural changes.
    - Added `Directory.Packages.props` for centralized package version control across the solution.
    - Updated various test project files to align with the new centralized version management approach and included asset management properties for testing-related packages.

--- src/DevServer.AppHost/Program.cs
+++ src/DevServer.AppHost/Program.cs
@@ -11,8 +11,6 @@ var envGateway = builder.AddProject<Projects.EnvironmentGateway_Api>("envGateway
     .WithScalar()
     .WithExternalHttpEndpoints();
 
-var weatherApi = builder.AddProject<Projects.WeatherForecastApi>("weatherApi")
-    .WithExternalHttpEndpoints();
 
 builder.AddNpmApp("adminFrontend","../admin-frontend")
     .WithReference(envGateway)
@@ -20,17 +18,5 @@ builder.AddNpmApp("adminFrontend","../admin-frontend")
     .WithHttpEndpoint(env: "ADMIN_FRONTEND_PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
-
-builder.AddNpmApp("devServerClient", "../DevServerClient")
-    .WithReference(weatherApi)
-    .WaitFor(weatherApi)
-    .WithReference(envGateway)
-    .WaitFor(envGateway)
-    .WithHttpEndpoint(env: "DEV_SERVER_CLIENT_PORT")
-    .WithExternalHttpEndpoints()
-    .PublishAsDockerFile();
-
-
-
 
 await builder.Build().RunAsync();
