8255d6f2d9c48709643ef5e5074b1526d5d4aab5
Michael Koch <m.koch@neuko-devment.com>
2025-06-17 14:50:59 +0200

    Add UserManager infrastructure and API projects
    This commit introduces a significant restructuring of the solution by adding new projects for `UserManager.Infrastructure` and `UserManager.Api`, while removing outdated references. Key features include:
    
    - New extension methods for user ID retrieval and password hashing.
    - JWT token creation and user context management.
    - Custom authorization attributes and handlers for permission checks.
    - Database context and initial migration setup for users and todos.
    - Updated dependency injection for new services and middleware configurations.
    - Enhanced API error handling and result management.
    - Configuration files for application settings and Swagger integration.
    
    Overall, these changes improve the organization, security, and functionality of the application.

---
+++ src/UserManager/UserManager.Infrastructure/Database/Schemas.cs
@@ -0,0 +1,6 @@
+﻿namespace UserManager.Infrastructure.Database;
+
+internal static class Schemas
+{
+    public const string Default = "public";
+}
---
+++ src/UserManager/UserManager.Infrastructure/Time/DateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿using SharedKernel;
+
+namespace UserManager.Infrastructure.Time;
+
+internal sealed class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
