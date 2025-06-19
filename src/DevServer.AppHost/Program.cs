b8ae32fdde58f9d36aa84ae4443a13e361a23551
Michael Koch <m.koch@neuko-devment.com>
2025-06-19 16:34:18 +0200

    Update database configuration and code quality settings
    - Modified `.editorconfig` to refine unused parameters setting.
    - Enhanced `Program.cs` to configure a PostgreSQL database server and updated gateway references.
    - Added `GatewayDbServer` section in `appsettings.Development.json` for volume path configuration.

--- src/DevServer.AppHost/Program.cs
+++ src/DevServer.AppHost/Program.cs
@@ -1,20 +1,32 @@
 using System.Diagnostics;
 using DevServer.AppHost;
 using k8s.Models;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var gatewaysDbServer = builder.AddPostgres(
+        name: "GatewaysDbServer")
+    .WithPgAdmin()
+    .WithDataBindMount(
+        source: builder.Configuration.GetValue<string>("GatewayDbServer:GatewayDbServerVolume")!,
+        isReadOnly: false)
+    .WithHttpEndpoint(port: 9001, name: "GatewaysDb", isProxied: false);
 
-var envGateway = builder.AddProject<Projects.EnvironmentGateway_Api>("envGateway")
+var productionGatewayDb = gatewaysDbServer.AddDatabase("ProductionGatewayDb");
+
+var productionGateway = builder.AddProject<Projects.EnvironmentGateway_Api>("ProductionGateway")
     .WithScalar()
+    .WithReference(productionGatewayDb)
+    .WaitFor(productionGatewayDb)
     .WithExternalHttpEndpoints();
 
 
 builder.AddNpmApp("adminFrontend","../admin-frontend")
-    .WithReference(envGateway)
-    .WaitFor(envGateway)
+    .WithReference(productionGateway)
+    .WaitFor(productionGateway)
     .WithHttpEndpoint(env: "ADMIN_FRONTEND_PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
