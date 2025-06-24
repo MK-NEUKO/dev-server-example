a5b44bd2a477ca870516acc5a2aa82824dfdc5a9
Michael Koch <m.koch@neuko-devment.com>
2025-06-24 18:25:26 +0200

    Add layout structure with header, footer, and theme toggler
    This commit introduces a new layout structure by adding a header and footer component, along with a theme toggler for light and dark modes. The CSS has been updated to utilize flexbox for a responsive design.
    
    The `ThemeService` manages the theme state and persists it in local storage. Unit tests have been added for the new components and services to ensure functionality and reliability.

---
+++ src/service-frontend/src/app/services/theme/theme.service.ts
@@ -0,0 +1,26 @@
+import { effect, Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  themeSignal = signal<string>(
+    JSON.parse(window.localStorage.getItem('themeSignal') ?? 'null')
+  );
+
+  updateDarkMode() {
+    this.themeSignal.update((value) => (value === 'dark' ? 'light' : 'dark'));
+    this.applyTheme();
+  }
+
+  constructor() {
+    effect(() => {
+      window.localStorage.setItem('themeSignal', JSON.stringify(this.themeSignal()))
+    });
+  }
+
+  applyTheme() {
+    const theme = this.themeSignal() === 'dark' ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-neuko-theme', theme);
+  }
+}
