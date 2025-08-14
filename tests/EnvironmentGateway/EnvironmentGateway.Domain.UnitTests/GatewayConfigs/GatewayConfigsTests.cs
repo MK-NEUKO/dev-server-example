55c7c45deee80c752a3cfec79682b14cc77abb3a
Michael Koch <m.koch@neuko-devment.com>
2025-08-14 12:59:59 +0200

    test: Add tests for GatewayConfig functionality
    - Added new tests for adding clusters and routes.
    - Introduced constants for test setup consistency.
    - Updated existing tests to use CreateTestConfig method.
    - Added assertions for null argument handling in AddCluster and AddRoute methods.
    
    Changes made by Michael Koch.

--- tests/EnvironmentGateway/EnvironmentGateway.Domain.UnitTests/GatewayConfigs/GatewayConfigsTests.cs
+++ tests/EnvironmentGateway/EnvironmentGateway.Domain.UnitTests/GatewayConfigs/GatewayConfigsTests.cs
@@ -1,6 +1,7 @@
-﻿using System.ComponentModel;
+﻿using EnvironmentGateway.Domain.Clusters;
 using EnvironmentGateway.Domain.GatewayConfigs;
 using EnvironmentGateway.Domain.GatewayConfigs.Events;
+using EnvironmentGateway.Domain.Routes;
 using EnvironmentGateway.Domain.UnitTests.Infrastructure;
 using FluentAssertions;
 
@@ -8,28 +9,89 @@ namespace EnvironmentGateway.Domain.UnitTests.GatewayConfigs;
 
 public class GatewayConfigsTests : BaseTest
 {
+    private const string TestClusterName = "TestCluster";
+    private const string TestDestinationName = "TestDestination";
+    private const string TestDestinationAddress = "http://test-address.com";
+    private const string TestRouteName = "TestRoute";
+    private const string TestRoutePath = "/test";
+
     [Fact]
     public void CreateConfig_Should_SetPropertyValues()
     {
         // Arrange & Act
-        var newConfig = GatewayConfig.Create();
+        var gatewayConfig = CreateTestConfig();
 
         // Assert
-        newConfig.Name?.Value.Should().Be(ConfigDefaultParams.ConfigName);
-        newConfig.IsCurrentConfig.Should().BeTrue();
-        newConfig.Routes.Count.Should().BeGreaterThanOrEqualTo(1);
-        newConfig.Clusters.Count.Should().BeGreaterThanOrEqualTo(1);
+        gatewayConfig.Name?.Value.Should().Be(ConfigDefaultParams.ConfigName);
+        gatewayConfig.IsCurrentConfig.Should().BeTrue();
+        gatewayConfig.Routes.Count.Should().BeGreaterThanOrEqualTo(1);
+        gatewayConfig.Clusters.Count.Should().BeGreaterThanOrEqualTo(1);
     }
 
     [Fact]
     public void CreateConfig_Should_RaiseInitialConfigCreatedDomainEvent()
     {
         // Act
-        var initialConfig = GatewayConfig.Create();
+        var gatewayConfig = CreateTestConfig();
 
         // Assert
-        NewConfigCreatedDomainEvent domainEvent = AssertDomainEventWasPublished<NewConfigCreatedDomainEvent>(initialConfig);
-        domainEvent.ConfigurationId.Should().Be(initialConfig.Id);
+        NewConfigCreatedDomainEvent domainEvent = AssertDomainEventWasPublished<NewConfigCreatedDomainEvent>(gatewayConfig);
+        domainEvent.ConfigurationId.Should().Be(gatewayConfig.Id);
     }
 
+    [Fact]
+    public void AddCluster_Should_AddClusterToList()
+    {
+        // Arrange
+        var gatewayConfig = CreateTestConfig();
+        var cluster = Cluster.Create(TestClusterName, TestDestinationName, TestDestinationAddress);
+
+        // Act
+        gatewayConfig.AddCluster(cluster);
+
+        // Assert
+        gatewayConfig.Clusters.Should().Contain(cluster);
+    }
+
+    [Fact]
+    public void AddRoute_Should_AddRouteToList()
+    {
+        // Arrange
+        var gatewayConfig = CreateTestConfig();
+        var route = Route.Create(TestRouteName, TestClusterName, TestRoutePath);
+
+        // Act
+        gatewayConfig.AddRoute(route);
+
+        // Assert
+        gatewayConfig.Routes.Should().Contain(route);
+    }
+
+    [Fact]
+    public void AddCluster_Should_ThrowArgumentNullException_WhenClusterIsNull()
+    {
+        // Arrange
+        var gatewayConfig = CreateTestConfig();
+
+        // Act
+        Action act = () => gatewayConfig.AddCluster(null!);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void AddRoute_Should_ThrowArgumentNullException_WhenRouteIsNull()
+    {
+        // Arrange
+        var gatewayConfig = CreateTestConfig();
+
+        // Act
+        Action act = () => gatewayConfig.AddRoute(null!);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    private static GatewayConfig CreateTestConfig() => GatewayConfig.Create();
 }
