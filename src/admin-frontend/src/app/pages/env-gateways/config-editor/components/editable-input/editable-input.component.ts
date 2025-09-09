6c3b36d9d6f5bab9797b975cdb80cce979e53546
Michael Koch <m.koch@neuko-devment.com>
2025-09-09 14:22:56 +0200

    feat: Enhance editable input component
    - clusters.component.html: Renamed index variable to `clusterIndex` for clarity and updated related bindings.
    - editable-input.component.css: Added new styles for layout improvements and focus states for better user feedback.
    - editable-input.component.html: Restructured component with a new wrapper, added accessibility label, and conditional edit button.
    - editable-input.component.ts: Introduced `isInputEditable` state, focus event handlers, and edit button management.
    
    Changes made by Michael Koch.

--- src/admin-frontend/src/app/pages/env-gateways/config-editor/components/editable-input/editable-input.component.ts
+++ src/admin-frontend/src/app/pages/env-gateways/config-editor/components/editable-input/editable-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, OnInit } from '@angular/core';
+import { Component, ElementRef, input, OnInit, ViewChild } from '@angular/core';
 import { CONFIG_EDITOR_CONTROL_NAMES } from '../../shared/config-editor-control-names';
 import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -18,13 +18,29 @@ export class EditableInputComponent implements OnInit {
   public readonly CONTROL_NAMES = CONFIG_EDITOR_CONTROL_NAMES;
   public readonly parent = input.required<AbstractControl<any, any> | null>();
   public readonly parentIndex = input.required<number>();
+  @ViewChild('editButton') editButton!: ElementRef<HTMLButtonElement>;
 
   public formControl!: FormControl;
   public parentFormGroup!: FormGroup;
+  public isInputEditable = false;
 
   ngOnInit(): void {
     this.formControl = this.parent()?.get(this.CONTROL_NAMES.CLUSTER_NAME) as FormControl;
     this.parentFormGroup = this.parent() as FormGroup;
   }
 
+  public onInputControlFocus() {
+    this.isInputEditable = true;
+  }
+
+  public onInputControlBlur() {
+    if (this.editButton.nativeElement.matches(':hover')) {
+      return;
+    }
+    this.isInputEditable = false;
+  }
+
+  public openEditingModal() {
+    console.log('Open Modal');
+  }
 }
