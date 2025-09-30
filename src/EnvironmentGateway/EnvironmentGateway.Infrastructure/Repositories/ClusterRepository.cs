f7be29f8ad830ee152b68356aaf67f594539123d
Michael Koch <m.koch@neuko-devment.com>
2025-09-30 11:18:20 +0200

    feat: Add ClusterRepository implementation
    - Repository: Introduced `ClusterRepository` class in the `EnvironmentGateway.Infrastructure.Repositories` namespace.
    - Namespace: Added `using EnvironmentGateway.Domain.Clusters` for domain references.
    - Design: Marked `ClusterRepository` as `internal sealed`, inheriting from `Repository<Cluster>` and implementing `IClusterRepository`.
    
    Changes made by Michael Koch.

---
+++ src/EnvironmentGateway/EnvironmentGateway.Infrastructure/Repositories/ClusterRepository.cs
@@ -0,0 +1,10 @@
+﻿using EnvironmentGateway.Domain.Clusters;
+
+namespace EnvironmentGateway.Infrastructure.Repositories;
+
+internal sealed class ClusterRepository(
+    EnvironmentGatewayDbContext dbContext)
+    : Repository<Cluster>(dbContext), IClusterRepository
+{
+    
+}
